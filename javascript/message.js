cff87956098e3e8ab184e8ac7ed2123e56d4364c
zhiqing <i@zhiqing.info>
2016-05-25 19:33:34 +0800

    rm about and message

--- javascript/message.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	alert("bbb");
-	var url = "http://api.duoshuo.com/threads/listPosts.jsonp?short_name=zhiqinglee&thread_key=message&page=1&limit=20";
-	$.getJSON(url + "&callbak=?",
-		function (data) {
-			alert("aaa");
-			alert(data.message);
-		});
-});
