48710e6700362213cc4aff8c0b2994b89a8d0591
zhiqing <lizhiqing1996@gmail.com>
2016-07-18 18:58:19 +0800

    remove lib.css

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,14 @@
-printer.str = '$ echo welcome to zhiqing.info\n'+
-			'$ cat about.me\n'+
-			'$ I am a student.\n'+
-			'$ My major is computer science.\n'+
-			'$ I am study in SUSE.\n'+
-			'$ I like movies,music and reading.\n'+
-			'$ If you want to contact me,\n'+
-			'$ You can send email to i@zhiqing.info\n'+
-			'$ echo Thanks!\n'+
-			'$ exit';
-printer.toId = 'drawing-board';
-printer.start();
+window.onload=function(){
+	printer.str = '$ echo welcome to zhiqing.info\n'+
+				'$ cat about.me\n'+
+				'$ I am a student.\n'+
+				'$ My major is computer science.\n'+
+				'$ I am study in SUSE.\n'+
+				'$ I like movies,music and reading.\n'+
+				'$ If you want to contact me,\n'+
+				'$ You can send email to i@zhiqing.info\n'+
+				'$ echo Thanks!\n'+
+				'$ exit';
+	printer.toId = 'drawing-board';
+	printer.start();
+}
